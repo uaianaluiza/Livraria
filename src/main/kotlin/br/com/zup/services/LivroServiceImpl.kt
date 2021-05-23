53428305fb2a9e83cecf1e42b8e571862124532a
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-23 16:16:47 -0300

    Criar Livro Service Impl

--- src/main/kotlin/br/com/zup/services/LivroServiceImpl.kt
+++ src/main/kotlin/br/com/zup/services/LivroServiceImpl.kt
@@ -1,4 +1,19 @@
 package br.com.zup.services
 
-interface LivroServiceImpl {
-}
+import br.com.zup.model.Livro
+import br.com.zup.repositories.LivroRepository
+import javax.inject.Inject
+
+class LivroServiceImpl (@Inject private var livroRepository: LivroRepository)
+    :LivroService{
+
+
+    override fun create(livro: Livro): Livro {
+        return livroRepository.save(livro)
+    }
+
+    override fun getAll():List<Livro> {
+        return livroRepository.findAll()
+    }
+
+    }
