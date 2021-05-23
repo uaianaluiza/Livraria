388dd19a4da2344e5f364e38ef2e36d76b55fa1c
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-23 16:29:29 -0300

    Criar métodos

--- src/main/kotlin/br/com/zup/services/LivroServiceImpl.kt
+++ src/main/kotlin/br/com/zup/services/LivroServiceImpl.kt
@@ -2,6 +2,7 @@ package br.com.zup.services
 
 import br.com.zup.model.Livro
 import br.com.zup.repositories.LivroRepository
+import java.lang.RuntimeException
 import javax.inject.Inject
 
 class LivroServiceImpl (@Inject private var livroRepository: LivroRepository)
@@ -16,4 +17,20 @@ class LivroServiceImpl (@Inject private var livroRepository: LivroRepository)
         return livroRepository.findAll()
     }
 
+    override fun delete(id: Long) {
+        if (livroRepository.existsById(id)){
+            livroRepository.deleteById(id)
+        }else{
+          throw RuntimeException ("Livro não encontrado")
+        }
+    }
+
+    override fun getById(id: Long): Livro? {
+        val livro = livroRepository.findById(id)
+
+        if (livro.isPresent){
+            return livro.get()
+        }
+        throw RuntimeException ("Livro não encontrado")
+    }
     }
