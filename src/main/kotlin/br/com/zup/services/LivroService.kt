4fedb06bc7539956400dfe7caafcdc1f0c9a19a0
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-23 16:16:17 -0300

    Criar Livro Service

--- src/main/kotlin/br/com/zup/services/LivroService.kt
+++ src/main/kotlin/br/com/zup/services/LivroService.kt
@@ -1,4 +1,13 @@
 package br.com.zup.services
 
-class LivroService {
+import br.com.zup.model.Livro
+import javax.inject.Singleton
+
+@Singleton
+interface LivroService {
+    fun create(livro: Livro):Livro
+    fun getAll(): List<Livro>
+    fun getById(id: Long):Livro?
+    fun delete(id:Long)
+
 }
